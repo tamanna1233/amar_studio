be5e219e678f25994d4269d365ae6441c2920b2a
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-09-10 14:11:54 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
 
 import React, { lazy } from 'react'
 const Home=lazy(()=> import ('./components/Home')) 
 const App = () => {
   return (
-    <div className='bg-black'>
+    <div className='bg-black '>
              <Navbar/>
-             <Home/>
+             <Outlet/>
     </div>
   )
 }
---
+++ src/router.jsx
@@ -0,0 +1,28 @@
+
+import { lazy } from "react"
+import {createBrowserRouter} from "react-router-dom"
+
+
+const App =lazy(()=>import("./App"))
+const Home =lazy(()=>import("./components/Home.jsx"))
+const About=lazy(()=>import("./components/About.jsx"))
+const router=createBrowserRouter([
+    {
+        path:"/",
+        element:<App/>,
+        children:[
+            {
+                path:"/",
+                element:<Home/>
+
+            },
+            {
+                path:"/About",
+                element:<About/>
+
+            }
+        ]
+    }
+])
+
+export default router
