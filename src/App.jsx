26b5980ee28d6a4dd5daa9c97eb6ece51552360e
TAMANNA <tamannakarthol320@gmail.com>
2024-09-10 08:31:25 -0700

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-import AboutUs from './components/AboutUs'
 import Gallery from './components/Gallery'
-=======
 import { Outlet } from 'react-router-dom'
->>>>>>> 880e114e696e0d5364f5af7167a588f646ec57c0
 import Navbar from './components/Navbar'
 
 import React, { lazy } from 'react'
@@ -12,14 +8,10 @@ const App = () => {
   return (
     <div className='bg-black '>
              <Navbar/>
-<<<<<<< HEAD
              <Home/>
              <Gallery/>
              
-             <AboutUs/>
-=======
              <Outlet/>
->>>>>>> 880e114e696e0d5364f5af7167a588f646ec57c0
     </div>
   )
 }
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import image from '../assets/logo/Group 9.png'
+import image2 from '../assets/Gallery/image.png'
+import image1 from '../assets/Gallery/image (1).png'
+import image3 from '../assets/Gallery/image (2).png'
+import image4 from '../assets/Gallery/image (3).png'
+import image5 from '../assets/Gallery/image (4).png'
+import image6 from '../assets/Gallery/image (5).png'
+import image7 from '../assets/Gallery/image (6).png'
 const Gallery = () => {
 const [imagePath,SetimagePath]= useState([])
 useEffect(()=>{
@@ -22,14 +28,14 @@ useEffect(()=>{
     <>
     <h1 className='text-yellow-300 text-center text-4xl p-4'>Gallery</h1>
 <div className='grid grid-cols-4 text-white  gap-2 mx-10 text-center  '>
-    <div className='bg-pink-200 '><img src={image} alt="" /></div>
-    <div className='bg-blue-200'></div>
-    <div className='bg-red-200'></div>
-    <div className='bg-yellow-200'></div>
-    <div className='bg-green-200 '></div>
-    <div className='bg-white'></div>
-    <div className='bg-orange-200'></div>
-    <div className='bg-gray-200'></div>
+    <div className='bg-pink-200 '><img src={image1} alt="" /></div>
+    <div className='bg-blue-200'><img src={image2} alt="" /></div>
+    <div className='bg-red-200'><img src={image2} alt="" /></div>
+    <div className='bg-yellow-200'><img src={image3} alt="" /></div>
+    <div className='bg-green-200 '><img src={image4} alt="" /></div>
+    <div className='bg-white'><img src={image5} alt="" /></div>
+    <div className='bg-orange-200'><img src={image6} alt="" /></div>
+    <div className='bg-gray-200'><img src={image7} alt="" /></div>
 </div>
       
     </>
