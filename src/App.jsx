247547e90766bf4222b6f3da77a0d04dbf505965
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-09-12 19:02:31 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import React, { lazy, useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Loading from './components/Loading';
 import Footer from './components/Footer';
-
+import { Helmet } from 'react-helmet';
 // const Home = lazy(() => import('./components/Home'));
 // const Gallery = lazy(() => import('./components/Gallery'));
 
 const App = () => {
 return (
+  
     <div className="bg-black  min-h-screen">
+      <Helmet>
+        <title>Amar Studio | Professional Photography Services</title>
+        <meta
+          name="description"
+          content="Amar Studio offers professional photography and videography services, including wedding, baby, pre-wedding, retirement, drone, and cinematic videography. Capture your special moments with our expert team."
+        />
+        <meta name="keywords" content="photography, wedding photography, baby photography, pre-wedding, retirement photos, drone photography, cinematic videography" />
+        <meta name="author" content="Amar Studio" />
+        <meta name="robots" content="index, follow" />
+      </Helmet>
       <Navbar />
       <Outlet />
       <Footer/>
