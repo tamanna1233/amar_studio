9140fe815fbaaf37689d29545d22b36e1d336049
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-09-13 13:29:20 +0530

    added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
      <div className='text-[#FFD700] text-lg sm:text-2xl md:text-4xl font-imprint'>About us</div> 
       <div className='flex-col sm:flex items-center justify-center py-8 sm:py-32 gap-y-8'>
      <div className=' sm:flex justify-center items-center px-4 gap-x-20'>
-     <img src={logo} alt=""  className='min-w-2/5 sm:w-2/5 h-auto ' loading='lazy'/>
+     <img src={logo} alt=""  className='min-w-2/5 sm:w-2/5 h-auto ' />
         <p className='text-white  font-poppin text-[0.64rem] sm:text-sm 5/6 sm:w-2/6 py-4  '>At amar studio, we are passionate about capturing the essence of life’s most precious moments. With years of experience in the photography industry, our team is dedicated to providing personalized and professional services tailored to your needs. From stunning wedding photos to intimate maternity sessions, we offer a wide range of photography services to help you relive your favorite moments.</p>
      </div>
      <div className=' sm:flex justify-center items-center px-4 gap-x-20'>
